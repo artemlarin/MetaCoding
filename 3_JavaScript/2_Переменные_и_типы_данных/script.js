ab759d99a7f6322aa2ab8b073a758f144dc4dac9
Artem <32986408+artemlarin@users.noreply.github.com>
2023-10-10 17:53:19 +0600

    Переменные и типы данных

---
+++ 3_JavaScript/2_Переменные_и_типы_данных/script.js
@@ -0,0 +1,26 @@
+// Создание переменных разных типов данных
+let name = "Мурат"; // Строка
+let number = 24;
+let isGreat = 4 > 1;
+let myArray = [1, 2, 3, 4, 5, 6, 7];
+let myObject = 
+{
+    name: 'Остров Проклятых',
+    author: 'Деннис Лихэйн',
+    pages: 270
+};
+
+// Вывести значение переменных на страницу
+document.write("Строка" + " " + ":" + " " + name);
+document.write('<br>')
+document.write('<br>')
+document.write("Число" + " " + ":" + " " + number);
+document.write('<br>')
+document.write('<br>')
+document.write("Булевое значение" + " " + ":" + " " + isGreat);
+document.write('<br>')
+document.write('<br>')
+document.write("Массив" + " " + ":" + " " + myArray);
+document.write('<br>')
+document.write('<br>')
+document.write("Объект" + " " + ":" + " " + myObject);
