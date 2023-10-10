ece7b8a27ba30c551d85ecb5a80399ee7c4d6d2c
Artem <32986408+artemlarin@users.noreply.github.com>
2023-10-10 18:39:24 +0600

    Консоль браузера и console.log

---
+++ 3_JavaScript/4_Консоль_браузера_и_console.log/script.js
@@ -0,0 +1,18 @@
+// Создание переменных разных типов данных
+let name = "Мурат"; // Строка
+let number = 24;
+let isGreat = 4 > 1;
+let myArray = [1, 2, 3, 4, 5, 6, 7];
+let myObject = 
+{
+    name: 'Остров Проклятых',
+    author: 'Деннис Лихэйн',
+    pages: 270
+};
+
+// Вывести значение переменных на страницу
+console.log("Строка" + " " + ":" + " " + name);
+console.log("Число" + " " + ":" + " " + number);
+console.log("Булевое значение" + " " + ":" + " " + isGreat);
+console.log("Массив" + " " + ":" + " " + myArray);
+console.log("Объект" + " " + ":" + " " + myObject);
