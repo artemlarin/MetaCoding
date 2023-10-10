7412694f404e9ed9774ad32d163301ff13a7686e
Artem <32986408+artemlarin@users.noreply.github.com>
2023-10-10 18:34:32 +0600

    Математические операции

---
+++ 3_JavaScript/3_Математические_операции/script.js
@@ -0,0 +1,23 @@
+// Выполнение математических операций и сохранение результатов в переменных
+let x = 6;
+let y = 2;
+
+// Вывести значение переменных на страницу
+document.write("Результат сложения + " + " " + ":" + " " + x + " + " + y + " = " + (x + y));
+document.write('<br>');
+document.write('<br>');
+document.write("Результат вычитания - " + " " + ":" + " " + x + " - " + y + " = " + (x - y));
+document.write('<br>');
+document.write('<br>');
+document.write("Результат умжножения * " + " " + ":" + " " + x + " * " + y + " = " + (x * y));
+document.write('<br>');
+document.write('<br>');
+document.write("Результат деления / " + " " + ":" + " " + x + " / " + y + " = " + (x / y));
+document.write('<br>');
+document.write('<br>');
+document.write("Результат взятие остатка от деления % " + " " + ":" + " " + x + " % " + y + " = " + (x % y));
+document.write('<br>');
+document.write('<br>');
+document.write("Результат возведение в степень ** " + " " + ":" + " " + x + " ** " + y + " = " + (x ** y));
+document.write('<br>');
+document.write('<br>');
