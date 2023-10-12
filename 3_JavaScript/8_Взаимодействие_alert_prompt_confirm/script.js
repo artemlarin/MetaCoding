de53a2c9fb76cd01dc59a121df49ade099a5b29e
Artem <32986408+artemlarin@users.noreply.github.com>
2023-10-12 20:13:16 +0600

    Взаимодействие alert prompt confirm

---
+++ 3_JavaScript/8_Взаимодействие_alert_prompt_confirm/script.js
@@ -0,0 +1,14 @@
+let age = prompt('Укажите ваш возраст', '');
+
+age = parseInt(age);
+alert('Ваш возраст через 10 лет: ' + (age + 10));
+
+let num1 = prompt('Введите первое число', '');
+let num2 = prompt('Введите второе число', '');
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+alert('Сумма введенных чисил: ' + (num1 + num2));
+
+let name = prompt('Введите имя и фамилию', '');
+alert('Здравствуйте, ' + name + '!');
